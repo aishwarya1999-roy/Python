49f7f5ad8c5375d71e56bbd4f2dc4001c69db37f
Aishwarya Roy <76627873+aishwarya1999-roy@users.noreply.github.com>
2022-01-28 02:58:41 +0530

    Add files via upload

---
+++ Sum_of_element_in_list/main.py
@@ -0,0 +1,13 @@
+# sum of elements in a list
+
+z = 0  # just let the value of a variable 0
+
+# creating a list
+ar = [5, 3, 10, 15, 27, 60]
+
+for i in ar:
+    z = z + i  # as z=0, z=0+9=9, then z=9 , z=9+41=50 and so on
+
+print("Sum of all elements in that list = ", z)
+
+""" I thing it is the easiest way """
